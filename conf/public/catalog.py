289d637eee6e865b312485198f31abe0a3af6d49
idartsdev9005@gmail.com <idartsdev9005@gmail.com>
2020-07-26 21:30:50 +0530

    changes

--- conf/public/catalog.py
+++ conf/public/catalog.py
@@ -9,21 +9,21 @@ TELEGRAM_QUESTION = "telegram question"
 
 #----------------------input files-------------------------
 
-EXCEL_SHEET = "/home/ec2-user/rmm_entity_extraction/data/03_model_inputs/Hari Mobile chatbot.xlsx"
+EXCEL_SHEET = "/home/ec2-user/rmm_entity_extraction/data_rmm_rmm/Inputs/Hari Mobile chatbot.xlsx"
 
-brand_model_csv = "/home/ec2-user/rmm_entity_extraction/data/03_model_inputs/brand_model_2.csv"
+brand_model_csv = "/home/ec2-user/rmm_entity_extraction/data_rmm/Inputs/brand_model_2.csv"
 
-alternative_name_csv = "/home/ec2-user/rmm_entity_extraction/data/03_model_inputs/alternative_name.csv"
+alternative_name_csv = "/home/ec2-user/rmm_entity_extraction/data_rmm/Inputs/alternative_name.csv"
 
-brand_rank_csv = "/home/ec2-user/rmm_entity_extraction/data/03_model_inputs/brand_rank.csv"
+brand_rank_csv = "/home/ec2-user/rmm_entity_extraction/data_rmm/Inputs/brand_rank.csv"
 
 #-------------------output files-------------------------
 
-CSV_OUTPUT_BRAND_MODEL = "/home/ec2-user/rmm_entity_extraction/data/05_output/brand_model.csv"
+CSV_OUTPUT_BRAND_MODEL = "/home/ec2-user/rmm_entity_extraction/data_rmm/Outputs/brand_model.csv"
 
-CSV_OUTPUT_BRAND_RANK = "/home/ec2-user/rmm_entity_extraction/data/05_output/brand_rank.csv"
+CSV_OUTPUT_BRAND_RANK = "/home/ec2-user/rmm_entity_extraction/data_rmm/Outputs/brand_rank.csv"
 
-CSV_OUTPUT_ALTERNATIVE_NAME = "/home/ec2-user/rmm_entity_extraction/data/05_output/alternative_name.csv"
+CSV_OUTPUT_ALTERNATIVE_NAME = "/home/ec2-user/rmm_entity_extraction/data_rmm/Outputs/alternative_name.csv"
 
 #--------------database config--------------------------
 
@@ -49,13 +49,13 @@ LONGITUDE_COLUMN = "longitude"
 
 #-----------test class----------------------
 
-TEST_OUTPUT_BRAND_CSV = "/home/ec2-user/rmm_entity_extraction/src/app/data/output/test_output_brand.csv"
+TEST_OUTPUT_BRAND_CSV = "/home/ec2-user/rmm_entity_extraction/data_rmm/Outputs/test_output_brand.csv"
 
-TEST_OUTPUT_MODEL_CSV = "/home/ec2-user/rmm_entity_extraction/src/app/data/output/public/test_output_model.csv"
+TEST_OUTPUT_MODEL_CSV = "/home/ec2-user/rmm_entity_extraction/data_rmm/Outputs/public/test_output_model.csv"
 
-TEST_SET_BRAND = "/home/ec2-user/rmm_entity_extraction/src/app/data/input/test_set_brands2.csv"
+TEST_SET_BRAND = "/home/ec2-user/rmm_entity_extraction/data_rmm/Inputs/test_set_brands2.csv"
 
-TEST_SET_MODELS = "/home/ec2-user/rmm_entity_extraction/src/app/data/input/test_set_models2.csv"
+TEST_SET_MODELS = "/home/ec2-user/rmm_entity_extraction/data_rmm/Inputs/test_set_models2.csv"
 
 #--------parameters------------
 
